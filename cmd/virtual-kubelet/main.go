e6fad542857bcd4d5a214db15f3f6a3389b0059d
Brian Goff <cpuguy83@gmail.com>
2019-11-15 11:03:44 -0800

    Bump vk to 1.2.0 + other deps.

--- cmd/virtual-kubelet/main.go
+++ cmd/virtual-kubelet/main.go
@@ -33,7 +33,7 @@ import (
 var (
 	buildVersion = "N/A"
 	buildTime    = "N/A"
-	k8sVersion   = "v1.14.3" // This should follow the version of k8s.io/kubernetes we are importing
+	k8sVersion   = "v1.15.2" // This should follow the version of k8s.io/kubernetes we are importing
 )
 
 func main() {
